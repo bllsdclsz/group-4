06ad866594c949f0edf50939d02483dac115b0d1
ekolog <ekolog055@gmail.com>
2021-11-08 16:42:32 +0100

    action added on cards form

--- event.js
+++ event.js
@@ -1,59 +1,3 @@
-let timeZone = new Date().toJSON();
-let currentDateTime = document.getElementById("meeting-time");
-currentDateTime.value = timeZone
-   .split("")
-   .splice(0, timeZone.length - 8)
-   .join("");
-
-console.log(timeZone);
-console.log(timeZone.split("").slice(0, timeZone.lastIndexOf(":")).join(""));
-console.log(currentDateTime.value);
-let image = document.querySelector("img");
-
-let movies;
-const addMovieButton = document.getElementById("add_event");
-let somthing = [1, 2, 3, 4];
-let movieList = document.querySelector(".movie_list");
-
-fetch("https://api.themoviedb.org/3/movie/popular?api_key=aadc24f1dd4399ac50c5f4c872dff158")
-   .then((response) => response.json())
-   .then((data) => {
-      console.log(data.results);
-      movies = data.results;
-   })
-   .then(() => {
-      for (let movie of movies) {
-         createMovieCard(movie)
-      }
-   })
-   .catch((err) => {
-      console.error(err);
-   });
-
-function createMovieCard(pMovie) {
-   return (movieList.innerHTML += `<section class="card opacity-transition">
-            <img
-               src=https://image.tmdb.org/t/p/original${pMovie.poster_path}
-               alt="film"
-               class="opacity-transition"
-            />
-            <div class="card-description opacity-transition">
-               <div class="description">
-                  <h2>${pMovie.title}</h2>
-                  <p>${pMovie.overview}</p>
-                  <input
-                     type="datetime-local"
-                     id="meeting-time"
-                     name="meeting-time"
-                  />
-                  <button type="button">Book Now</button>
-               </div>
-            </div>
-          </section>`);
-}
-console.log(somthing);
-console.log(movies);
-
 const addEvent = (pDateTime, pMovieTime) => {
    let newEvent = {
       summary: "Sample Event for CINEMATIQ",
@@ -85,9 +29,4 @@ const addEvent = (pDateTime, pMovieTime) => {
    request.execute(function (event) {
       appendPre("Event created: " + event.htmlLink);
    });
-   console.log(currentDateTime.value);
 };
-
-addMovieButton.addEventListener("click", () => {
-   addEvent(currentDateTime.value, "01:00");
-});
---
+++ movies.js
@@ -0,0 +1,20 @@
+let movies;
+let movieList = document.querySelector(".movie_list");
+
+fetch("https://api.themoviedb.org/3/movie/popular?api_key=aadc24f1dd4399ac50c5f4c872dff158")
+   .then((response) => response.json())
+   .then((data) => {
+      console.log(data.results);
+      movies = data.results;
+   })
+   .then(() => {
+      for (let movie of movies) {
+         createMovieCard(movie);
+      }
+   })
+   .catch((err) => {
+      console.error(err);
+   });
+
+
+
